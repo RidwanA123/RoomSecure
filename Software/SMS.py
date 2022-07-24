7b40c2ff451c035899037b1a6edf4f6fcb3210ac
RidwanA123 <58993858+RidwanA123@users.noreply.github.com>
2022-07-24 04:18:53 -0400

    Update SMS.py

--- Software/SMS.py
+++ Software/SMS.py
@@ -7,8 +7,8 @@ import base64
 arduino = serial.Serial('COM4',9600,timeout=0.1)
 def send_sms():
     text = ("Someone or Something Entered your room! Go Check it out!")
-    account_sid = "AC1d20c171fb735b195dadb5144e1c7cfe"
-    auth_token = "e41f64ab63da5cca7b56795baad1e667"
+    account_sid = ""
+    auth_token = ""
     client = Client(account_sid, auth_token)
 
     message = client.messages \
